dd339aa815407ab558d8151a53db02f239764755
Shifunggoy <stayciee.canon26@gmail.com>
2024-07-19 11:44:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I have something for you, will you accept?";
+  gif.src ="kukuli-maymun-unscreen.gif";
+
+  yesBtn.addEventListener("click", () =>{
+    question.innerHTML = "Please check your email, bb!!";
+    gif.src ="monkey-kiss-unscreen.gif";
+  });
+
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
+
